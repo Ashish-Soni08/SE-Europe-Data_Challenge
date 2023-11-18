e2b499a95b2e45650b75d322bb6e68a5fb6ea831
Ashish Soni <ashish.soni2091@gmail.com>
2023-11-18 22:32:52 +0000

    data ingestion script

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,52 +1,49 @@
 import argparse
 import datetime
 import pandas as pd
+import logging
 from utils import perform_get_request, xml_to_load_dataframe, xml_to_gen_data
 
-def get_load_data_from_entsoe(regions, periodStart='202201010000', periodEnd='202212312300', output_path='./data'):
-    # URL of the RESTful API
-    url = 'https://web-api.tp.entsoe.eu/api'
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    # General parameters for the API
+def get_load_data_from_entsoe(regions, periodStart='202201010000', periodEnd='202212312300', output_path='./data'):
+    url = 'https://web-api.tp.entsoe.eu/api'
     params = {
         'securityToken': '1d9cd4bd-f8aa-476c-8cc1-3442dc91506d',
         'documentType': 'A65',
         'processType': 'A16',
-        'outBiddingZone_Domain': 'FILL_IN', # used for Load data
-        'periodStart': periodStart, # in the format YYYYMMDDHHMM
-        'periodEnd': periodEnd, # in the format YYYYMMDDHHMM
+        'outBiddingZone_Domain': 'FILL_IN',
+        'periodStart': periodStart,
+        'periodEnd': periodEnd,
     }
 
-    # Loop through the regions
     for region, area_code in regions.items():
-        print(f'Fetching load data for {region}...')
+        logging.info(f'Fetching load data for {region}...')
         params['outBiddingZone_Domain'] = area_code
 
         response_content = perform_get_request(url, params)
         df = xml_to_load_dataframe(response_content)
 
         df.to_csv(f'{output_path}/load_{region}.csv', index=False)
-        print(f"Finished Fetching Load Data for {region}")
-    print("Load Data fetched succesfully")
+        logging.info(f"Finished Fetching Load Data for {region}")
+    logging.info("Load Data fetched successfully")
 
 def get_gen_data_from_entsoe(regions, periodStart='202201010000', periodEnd='202212312300', output_path='./data'):
-    # URL of the RESTful API
     url = 'https://web-api.tp.entsoe.eu/api'
-
-    # General parameters for the API
     params = {
         'securityToken': '1d9cd4bd-f8aa-476c-8cc1-3442dc91506d',
         'documentType': 'A75',
         'processType': 'A16',
-        'outBiddingZone_Domain': 'FILL_IN', # used for Load data
-        'in_Domain': 'FILL_IN', # used for Generation data
-        'periodStart': periodStart, # in the format YYYYMMDDHHMM
-        'periodEnd': periodEnd, # in the format YYYYMMDDHHMM
+        'outBiddingZone_Domain': 'FILL_IN',
+        'in_Domain': 'FILL_IN',
+        'periodStart': periodStart,
+        'periodEnd': periodEnd,
     }
     green_energy_codes = ['B01', 'B09', 'B10', 'B11', 'B12', 'B13', 'B15', 'B16', 'B17', 'B18', 'B19']
-    # Loop through the regions
+
     for region, area_code in regions.items():
-        print(f'Fetching generation data for {region}...')
+        logging.info(f'Fetching generation data for {region}...')
         params['outBiddingZone_Domain'] = area_code
         params['in_Domain'] = area_code
 
@@ -54,14 +51,11 @@ def get_gen_data_from_entsoe(regions, periodStart='202201010000', periodEnd='202
         
         dfs = xml_to_gen_data(response_content)
         
-        # Save the dfs to CSV files
         for psr_type, df in dfs.items():
             if psr_type in green_energy_codes:
                 df.to_csv(f'{output_path}/gen_{region}_{psr_type}.csv', index=False)
-                print(f"Finished Fetching Data for {region} and {psr_type}")
-    print("Generation Data fetched succesfully")
-
-
+                logging.info(f"Finished Fetching Data for {region} and {psr_type}")
+    logging.info("Generation Data fetched successfully")
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Data ingestion script for Energy Forecasting Hackathon')
@@ -99,16 +93,7 @@ def main(start_time, end_time, output_path):
         'NE': '10YNL----------L',
     }
 
-    # Transform start_time and end_time to the format required by the API: YYYYMMDDHHMM
     start_time = start_time.strftime('%Y%m%d%H%M')
     end_time = end_time.strftime('%Y%m%d%H%M')
 
-    # Get Load data from ENTSO-E
     get_load_data_from_entsoe(regions, start_time, end_time, output_path)
-
-    # Get Generation data from ENTSO-E
-    get_gen_data_from_entsoe(regions, start_time, end_time, output_path)
-
-if __name__ == "__main__":
-    args = parse_arguments()
-    main(args.start_time, args.end_time, args.output_path)
