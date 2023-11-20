53dcc0dbced1357778efa6a4b6309bbbe96e567f
Ashish Soni <ashish.soni2091@gmail.com>
2023-11-20 23:29:58 +0000

    fill the script

--- src/model_training.py
+++ src/model_training.py
@@ -1,41 +1,48 @@
-import pandas as pd
 import argparse
+import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
 
 def load_data(file_path):
-    # TODO: Load processed data from CSV file
+    df = pd.read_csv(file_path, parse_dates=['start_time'], index_col='start_time')
     return df
 
 def split_data(df):
-    # TODO: Split data into training and validation sets (the test set is already provided in data/test_data.csv)
-    return X_train, X_val, y_train, y_val
+    
+    X_train = df.drop('label', axis=1)
+    y_train = df['label']
+    
+    return X_train, y_train
 
 def train_model(X_train, y_train):
-    # TODO: Initialize your model and train it
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
     return model
 
 def save_model(model, model_path):
-    # TODO: Save your trained model
-    pass
+    with open(model_path, 'wb') as file:
+        pickle.dump(model, file)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Model training script for Energy Forecasting Hackathon')
     parser.add_argument(
         '--input_file', 
         type=str, 
-        default='data/processed_data.csv', 
+        default='data/train.csv', 
         help='Path to the processed data file to train the model'
     )
     parser.add_argument(
         '--model_file', 
         type=str, 
-        default='models/model.pkl', 
+        default='models/random_forest_model.pkl', 
         help='Path to save the trained model'
     )
     return parser.parse_args()
 
 def main(input_file, model_file):
     df = load_data(input_file)
-    X_train, X_val, y_train, y_val = split_data(df)
+    X_train, y_train = split_data(df)
     model = train_model(X_train, y_train)
     save_model(model, model_file)
 
